60baa63629688c01da7dc4d2e7b73e203cb6213a
= <=>
2022-11-06 09:13:32 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import {BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import {Home} from './pages/home/home'
-import {Tickets} from './pages/tickets/tickets'
-
+import {Home} from './pages/home/home';
+import {Tickets} from './pages/tickets/tickets';
+import {TelaTv} from './pages/tv/tvPage'
+import {Rate} from './pages/rate/rate'
+ 
 
 export function App() {
 
@@ -9,10 +11,12 @@ export function App() {
     
     <Router>
     <Routes>
+      
       <Route path='/' element={<Home />} />
-      <Route path='/tickets' element={<Tickets />} />
-
-
+      <Route path='/tickets' element={<Tickets />}/>
+      <Route path='/rate' element={<Rate/>}/>
+      <Route path='/TelaTv' element={<TelaTv />}/>
+        
     </Routes>
   </Router>
   
